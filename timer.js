5fc5e50685ca139ba6c6fdbcc91480de8c684652
Ali Karbassi <ali@karbassi.com>
2017-04-03 14:34:04 -0500

    Added `addToTimer()` function to handle addition/subtraction to the timer.
    Converted single to double quotes as there was a mix and match of them.
    
    Cleaned up event listener functions.

--- timer.js
+++ timer.js
@@ -12,11 +12,11 @@ function $(str) {
     }
 }
 
-var sound = $('#beep');
+var sound = $("#beep");
 
 
 function getTime(str) {
-    if (str.indexOf(':') == -1) {
+    if (str.indexOf(":") == -1) {
         return parseInt(str, 10) * 1e3;
     }
 
@@ -28,7 +28,7 @@ function getTime(str) {
         1
     ];
 
-    var parts = str.split(':');
+    var parts = str.split(":");
     while (parts.length > 0) {
         ms += parts.pop() * multiples.pop()
     }
@@ -37,10 +37,10 @@ function getTime(str) {
 }
 
 function display(date) {
-    $('#hours').innerText = date.hours;
-    $('#minutes').innerText = date.minutes;
-    $('#seconds').innerText = date.seconds;
-    $('#microseconds').innerText = date.microseconds;
+    $("#hours").innerText = date.hours;
+    $("#minutes").innerText = date.minutes;
+    $("#seconds").innerText = date.seconds;
+    $("#microseconds").innerText = date.microseconds;
 
 }
 
@@ -65,38 +65,40 @@ function hmss(microseconds) {
     var x = microseconds / 1e3;
 
     // Microseconds
-    var ms = parseInt(((x % 1) * 1e3), 10) + '';
+    var ms = parseInt(((x % 1) * 1e3), 10) + "";
 
     while (ms.length < 3) {
-        ms += '0';
+        ms += "0";
     }
 
     date.microseconds = ms;
 
     // Seconds
-    var s = parseInt(x % 60, 10) + '';
-    date.seconds = '00'.substring(s.length) + s
+    var s = parseInt(x % 60, 10) + "";
+    date.seconds = "00".substring(s.length) + s
 
     // Minute
     x /= 60;
-    var m = parseInt(x % 60, 10) + '';
-    date.minutes = '00'.substring(m.length) + m
+    var m = parseInt(x % 60, 10) + "";
+    date.minutes = "00".substring(m.length) + m
 
     // Hours
     x /= 60;
-    var h = parseInt(x % 24, 10) + '';
-    date.hours = '00'.substring(h.length) + h
+    var h = parseInt(x % 24, 10) + "";
+    date.hours = "00".substring(h.length) + h
 
     // Days
     x /= 24;
-    var d = parseInt(x, 10) + '';
-    date.days = '00'.substring(d.length) + d
+    var d = parseInt(x, 10) + "";
+    date.days = "00".substring(d.length) + d
 
 
     return date;
 }
 
-
+/**
+ *
+ */
 function step() {
 
     if (isStopped) {
@@ -117,8 +119,26 @@ function step() {
         display(hmss(0));
 
         sound.play();
+        isStopped = true;
     }
 }
+
+/**
+ * Add `seconds` to current (or new) timer.
+ *
+ * @param {String} seconds
+ */
+function addToTimer(seconds) {
+    var ms = parseInt(seconds, 10) * 1e3;
+
+    if (!(end instanceof Date)) {
+        end = new Date();
+    }
+
+    end = new Date(end.getTime() + ms);
+    step();
+}
+
 
 /**
  * Stop Audio
@@ -150,40 +170,81 @@ function stop() {
  * Toggle Microseconds
  */
 function toggleMs() {
-    $('#microseconds').style.display = $('#microseconds').style.display === 'none' ? '' : 'none';
+    $("#microseconds").style.display = $("#microseconds").style.display === "none" ? "" : "none";
 }
 
 // Event Listeners
 
-$('body').addEventListener('dblclick', function () {
-    console.log('dblclick');
-    stopTimer();
-    stopSound();
-})
 
-window.addEventListener("hashchange", function (obj) {
-    var str = obj.target.window.location.hash.slice(1);
-    isStopped = false;
+
+/**
+ * Called when the body is double clicked
+ *
+ * @param {MouseEvent} event
+ */
+function onBodyDblclick(event) {
+    stop();
+}
+
+$("body").addEventListener("dblclick", onBodyDblclick);
+
+
+/**
+ * Called when page hash changes.
+ *
+ * @param {MouseEvent} event
+ */
+function onHashChange(event) {
+    var str = event.target.window.location.hash.slice(1);
     run(str);
-});
+}
 
-window.addEventListener("keydown", function (event) {
-  if (event.defaultPrevented) {
-    return; // Do nothing if the event was already processed
-  }
+window.addEventListener("hashchange", onHashChange);
 
-  switch (event.key) {
-    case ' ':
-        stopTimer();
-        stopSound();
-        break;
 
-    case 'm':
-        toggleMs();
-        break;
+/**
+ * Called whenever a keyboard key is pressed.
+ *
+ * @param {MouseEvent} event
+ */
+function onKeyPressed(event) {
+    if (event.defaultPrevented) {
+        return; // Do nothing if the event was already processed
+    }
 
-    default:
-        break;
-  }
+    switch (true) {
+        case event.key === " ":
+        case event.key === "0":
+            stop();
+            break;
 
-});
+        case event.key === "m":
+            toggleMs();
+            break;
+
+        // 49-57 is 1-9 on the top row
+        // 97-105 is 1-9 on the number keypad
+        case /(49|5[0-7])/.test(event.keyCode) || /(9[7-9]|10[0-5])/.test(event.keyCode):
+
+            // Depending on the number pressed (keypad or not), sub is the
+            // number subtracted from the keyCode to get a base 10 number
+            var sub = event.keyCode > 57 ? 96 : 48;
+            var numberPressed = event.keyCode - sub;
+            var time = numberPressed * 60;
+
+            // If shift is pressed, subtracted the number.
+            if (event.shiftKey) {
+                time *= -1;
+            }
+
+
+            isStopped = false;
+            addToTimer(time);
+            break;
+
+        default:
+            break;
+    }
+}
+
+window.addEventListener("keydown", onKeyPressed);
